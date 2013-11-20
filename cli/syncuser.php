6b78ad2d59a440bd59843db04565b091a1b181c3
Mark Nielsen <mark@moodlerooms.com>
2013-11-20 08:39:28 -0800

    INT-5183: Reduce number of calls to /blocks/gapps/rest.php
    Instead of mimicking asynchronous processes with HTTP, this
    user sync process actually asynchronously syncs users
    with PHP.  This will remove the load on the web servers.

---
+++ cli/syncuser.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Sync a single user to gapps
+ *
+ * @author Mark Nielsen
+ * @package block_gapps
+ */
+
+define('CLI_SCRIPT', true);
+
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+require_once($CFG->libdir.'/clilib.php');
+require_once($CFG->dirroot.'/local/mr/framework/bootstrap.php');
+require_once($CFG->dirroot.'/blocks/gapps/model/gsync.php');
+
+list($options, $unrecognized) = cli_get_params(
+    array('userid' => 0, 'help' => false),
+    array('u' => 'userid', 'h' => 'help')
+);
+
+if ($unrecognized) {
+    $unrecognized = implode("\n  ", $unrecognized);
+    cli_error(get_string('cliunknowoption', 'admin', $unrecognized));
+}
+
+if ($options['help']) {
+    echo "Gapps Sync User CLI Script.
+
+Options:
+-u, --userid  Sync this user to Google Apps
+-h, --help    Print out this help
+
+Example:
+/usr/bin/php blocks/gaps/cli/syncuser.php --userid=5
+";
+    die;
+}
+
+$gapps = new blocks_gapps_model_gsync();
+$gapps->sync_user_cli($options['userid']);
+exit(0);
