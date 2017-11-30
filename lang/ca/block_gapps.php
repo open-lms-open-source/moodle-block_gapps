00b3a64dcd20bbafcc6f91ddf21fc67a11b65e6e
Sam Chaffee <sam@moodlerooms.com>
2017-11-30 11:57:22 -0700

    INT-12686: Importing ca lang

---
+++ lang/ca/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Autoritzeu Gmail per visualitzar els missatges sense llegir.';
+$string['calendar'] = 'Calendari';
+$string['clientid'] = 'ID de client';
+$string['clientiddesc'] = 'Aquest és l\'ID de client del vostre compte de servei a Google Developers Console. Només el necessitareu si voleu que es mostrin els missatges de Gmail al bloc de Google Apps.';
+$string['compose'] = 'Redacta un correu electrònic';
+$string['domainnotconfigured'] = 'El domini de Google Apps no està configurat als paràmetres d\'administrador de Google Apps.';
+$string['domainsetting'] = 'Domini de Google Apps';
+$string['domainsettingdesc'] = 'Aquest és el domini associat amb el vostre compte de Google Apps. Per exemple, si inicieu la sessió a Google Apps com a \'foo@bar.com\', el vostre domini és \'bar.com\'.';
+$string['drive'] = 'Drive';
+$string['gapps:addinstance'] = 'Afegeix un nou bloc de Google Apps';
+$string['gapps:myaddinstance'] = 'Afegeix un nou bloc de Google Apps a La meva pàgina d\'inici';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Nombre de missatges sense llegir';
+$string['msgnumberunreadinfo'] = 'Nombre de missatges sense llegir que es mostraran al bloc de Google Apps';
+$string['newwinlink'] = 'Obre els enllaços en una finestra emergent';
+$string['newwinlinkinfo'] = 'Si activeu aquest paràmetre, tots els enllaços del bloc de Google Apps s\'obriran a una finestra nova.';
+$string['nosubject'] = 'Sense assumpte';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Mostra el nom';
+$string['showfirstnameinfo'] = 'Mostra el nom del remitent de Gmail al costat del missatge.';
+$string['showlastname'] = 'Mostra els cognoms';
+$string['showlastnameinfo'] = 'Mostra els cognoms del remitent de Gmail al costat del missatge.';
+$string['unreadmessages'] = 'Teniu {$a} missatges sense llegir a la bústia d\'entrada.';
