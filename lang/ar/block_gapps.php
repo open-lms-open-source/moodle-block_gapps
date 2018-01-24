4afd0fb5163a15f584c843eeac23ca0197206589
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'تخويل الوصول إلى Gmail لعرض رسائلك غير المقروءة هنا.';
+$string['calendar'] = 'التقويم';
+$string['clientid'] = 'معرف العميل';
+$string['clientiddesc'] = 'هذا هو معرف العميل الخاص بحساب الخدمة الموجود في "وحدة تحكم مطوري Google".  لا يكون ذلك ضروريًا إلا إذا كنت تريد عرض رسائل Gmail في كتلة Google Apps.';
+$string['compose'] = 'إنشاء رسالة';
+$string['domainnotconfigured'] = 'لم يتم تكوين مجال Google Apps في إعدادات مسؤول Google Apps.';
+$string['domainsetting'] = 'مجال Google Apps';
+$string['domainsettingdesc'] = 'يعتبر هذا المجال هو المجال المرتبط بحساب Google Apps الخاص بك. على سبيل المثال، إذا قمت بتسجيل الدخول إلى Google Apps كـ \'foo@bar.com\'، يكون مجالك هو \'bar.com\'.';
+$string['drive'] = 'محرك أقراص';
+$string['gapps:addinstance'] = 'إضافة كتلة Google Apps جديدة';
+$string['gapps:myaddinstance'] = 'إضافة كتلة Google Apps جديدة إلى الصفحة الرئيسية الخاصة بي';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'عدد الرسائل غير المقروءة';
+$string['msgnumberunreadinfo'] = 'عدد رسائل Gmail غير المقروءة المراد عرضها في كتلة Google Apps.';
+$string['newwinlink'] = 'فتح روابط في إطار منبثق';
+$string['newwinlinkinfo'] = 'سيتم فتح جميع الروابط الموجودة في كتلة Google Apps في نافذة جديدة في حالة تمكين هذا الإعداد.';
+$string['nosubject'] = 'بلا الموضوع';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'إظهار الاسم الأول';
+$string['showfirstnameinfo'] = 'إظهار الاسم الأول لمؤلف Gmail بجانب الرسالة الخاصة به.';
+$string['showlastname'] = 'إظهار الاسم الأخير';
+$string['showlastnameinfo'] = 'إظهار الاسم الأخير لمؤلف Gmail بجانب الرسالة الخاصة به.';
+$string['unreadmessages'] = 'لديك {$a} من الرسائل غير المقروءة في علبة الوارد.';
