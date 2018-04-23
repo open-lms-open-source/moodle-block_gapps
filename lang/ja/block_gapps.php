fb4786eb5eeed4c12fa4818bfb415a8376d404b0
David Castro <david.castro@blackboard.com>
2018-04-23 15:30:44 +0000

    INT-13192: Importing ja lang

---
+++ lang/ja/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = '未読メッセージをここに表示するため、Gmailへのアクセスを承認します。';
+$string['calendar'] = 'カレンダー';
+$string['clientid'] = 'クライアントID';
+$string['clientiddesc'] = 'Google Developers ConsoleのサービスアカウントのクライアントIDです。Google AppsブロックにGmailメッセージを表示する場合にのみ必要です。';
+$string['compose'] = 'メールを作成する';
+$string['domainnotconfigured'] = 'Google AppsドメインはGoogle Apps管理設定で構成されません。';
+$string['domainsetting'] = 'Google Appsドメイン';
+$string['domainsettingdesc'] = 'これは、Google Appsアカウントと関連付けられているドメインです。たとえば、\'foo@bar.com\'でGoogle Appsにログインしている場合、ドメインは\'bar.com\'です。';
+$string['drive'] = 'ドライブ';
+$string['gapps:addinstance'] = '新しいGoogle Appsブロックを追加する';
+$string['gapps:myaddinstance'] = '新しいGoogle Appsブロックをマイホームに追加する';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = '未読メッセージ件数';
+$string['msgnumberunreadinfo'] = 'Google Appsブロックに表示する未読Gmailメッセージの件数です。';
+$string['newwinlink'] = 'ポップアップにリンクを開く';
+$string['newwinlinkinfo'] = 'この設定を有効にした場合、Google Appsブロックのすべてのリンクが新規ウィンドウで開きます。';
+$string['nosubject'] = '件名なし';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = '名を表示する';
+$string['showfirstnameinfo'] = 'メッセージの横にGmail作成者の名を表示します。';
+$string['showlastname'] = '姓を表示する';
+$string['showlastnameinfo'] = 'メッセージの横にGmail作成者の姓を表示します。';
+$string['unreadmessages'] = '受信箱に {$a} 通の未読メッセージがあります。';
