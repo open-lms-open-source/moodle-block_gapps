73be38c2f318ab2cc368894e0c50bbf80c3109ba
David Castro <david.castro@blackboard.com>
2018-04-23 15:41:05 +0000

    INT-13192: Importing pl lang

---
+++ lang/pl/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Zezwalaj na dostęp do poczty Gmail, aby wyświetlić nieprzeczytane wiadomości tutaj.';
+$string['calendar'] = 'Kalendarz';
+$string['clientid'] = 'Identyfikator klienta';
+$string['clientiddesc'] = 'Jest to identyfikator klienta konta usługi w konsoli programisty Google. Jest on wymagany wyłącznie do wyświetlania wiadomości z poczty Gmail w bloku Google Apps.';
+$string['compose'] = 'Utwórz wiadomość e-mail';
+$string['domainnotconfigured'] = 'Nie skonfigurowano domeny Google Apps w ustawieniach administratora Google Apps.';
+$string['domainsetting'] = 'Domena Google Apps';
+$string['domainsettingdesc'] = 'Domena powiązana z kontem Google Apps. Przykładowo, jeżeli użytkownik loguje się do konta Google Apps jako „foo@bar.com”, domena użytkownika to „bar.com”.';
+$string['drive'] = 'Dysk';
+$string['gapps:addinstance'] = 'Dodaj nowy blok Google Apps';
+$string['gapps:myaddinstance'] = 'Dodaj nowy blok Google Apps do Mojej strony domowej';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Liczba nieprzeczytanych wiadomości';
+$string['msgnumberunreadinfo'] = 'Liczba nieprzeczytanych wiadomości, które mają być wyświetlane w bloku Google Apps.';
+$string['newwinlink'] = 'Otwieraj łącza w oknie podręcznym';
+$string['newwinlinkinfo'] = 'Jeżeli opcja zostanie włączona, wszystkie łącza w bloku Google Apps będą otwierać się w nowym oknie.';
+$string['nosubject'] = 'Brak tematu';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Pokaż imię';
+$string['showfirstnameinfo'] = 'Pokaż imię autora obok wiadomości Gmail.';
+$string['showlastname'] = 'Pokaż nazwisko';
+$string['showlastnameinfo'] = 'Pokaż nazwisko autora obok wiadomości Gmail.';
+$string['unreadmessages'] = 'Liczba nieprzeczytanych wiadomości w skrzynce: {$a}.';
