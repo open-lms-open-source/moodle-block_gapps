4eb3c8b1996e0517154e1dd228bc112cc7318908
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:36 +0000

    INT-13192: Importing nl lang

---
+++ lang/nl/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Sta toegang tot Gmail toe om je ongelezen berichten hier te lezen.';
+$string['calendar'] = 'Kalender';
+$string['clientid'] = 'Client-ID';
+$string['clientiddesc'] = 'Dit is de client-ID van je service-account in de Google Developers Console. Dit is alleen nodig als je Gmail-berichten in het Google Apps-blok wilt weergeven.';
+$string['compose'] = 'Nieuwe e-mail opstellen';
+$string['domainnotconfigured'] = 'Het Google Apps-domein is niet geconfigureerd in de Google Apps-beheerinstellingen.';
+$string['domainsetting'] = 'Google Apps-domein';
+$string['domainsettingdesc'] = 'Dit is het domein dat bij je Google Apps-account hoort. Als je je bijvoorbeeld aanmeldt bij Google Apps als \'foo@bar.com\', dan is je domein \'bar.com\'.';
+$string['drive'] = 'Drive';
+$string['gapps:addinstance'] = 'Voeg een nieuw Google Apps-blok toe';
+$string['gapps:myaddinstance'] = 'Voeg een nieuw Google Apps-blok toe aan Mijn startpagina';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Aantal ongelezen berichten';
+$string['msgnumberunreadinfo'] = 'Het aantal ongelezen Gmail-berichten dat je wilt weergeven in het Google Apps-blok.';
+$string['newwinlink'] = 'Koppelingen openen in pop-up';
+$string['newwinlinkinfo'] = 'Als deze instelling is ingeschakeld, worden alle koppelingen in het Google Apps-blok in een nieuw venster geopend.';
+$string['nosubject'] = 'Geen onderwerp';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Voornaam weergeven';
+$string['showfirstnameinfo'] = 'Toon de voornaam van de Gmail-auteur naast het betreffende bericht.';
+$string['showlastname'] = 'Achternaam weergeven';
+$string['showlastnameinfo'] = 'Toon de achternaam van de Gmail-auteur naast het betreffende bericht.';
+$string['unreadmessages'] = 'Je hebt {$a} ongelezen berichten in je inbox.';
