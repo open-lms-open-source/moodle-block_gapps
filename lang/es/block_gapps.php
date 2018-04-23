0f0611a90a17a0534754f994ce701a5034cf1ab4
David Castro <david.castro@blackboard.com>
2018-04-23 15:42:57 +0000

    INT-13192: Importing es lang

---
+++ lang/es/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Autorice el acceso a Gmail para ver sus mensajes sin leer aquí.';
+$string['calendar'] = 'Calendario';
+$string['clientid'] = 'ID de cliente';
+$string['clientiddesc'] = 'Este es el ID de cliente de su cuenta de servicio en la consola de desarrolladores de Google. Esto solo es necesario si desea mostrar los mensajes de Gmail en el block de Google Apps.';
+$string['compose'] = 'Redactar correo electrónico';
+$string['domainnotconfigured'] = 'El dominio de Google Apps no está configurado en los ajustes de administración de Google Apps.';
+$string['domainsetting'] = 'Dominio de Google Apps';
+$string['domainsettingdesc'] = 'Este es el dominio asociado con su cuenta de Google Apps. Por ejemplo, si inicia sesión en Google Apps como \'foo@bar.com\', el dominio es \'bar.com\'.';
+$string['drive'] = 'Unidad';
+$string['gapps:addinstance'] = 'Agregar un bloque nuevo de Google Apps';
+$string['gapps:myaddinstance'] = 'Agregar un bloque nuevo de Google Apps a Mi área personal';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Cantidad de mensajes sin leer';
+$string['msgnumberunreadinfo'] = 'La cantidad de mensajes sin leer para mostrar en el block de Google Apps.';
+$string['newwinlink'] = 'Abrir enlaces en nueva ventana';
+$string['newwinlinkinfo'] = 'Si se habilita este ajuste, todos los enlaces en el block de Google Apps se abrirán en una nueva ventana.';
+$string['nosubject'] = 'Sin asunto';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Mostrar nombre';
+$string['showfirstnameinfo'] = 'Mostrar el nombre del autor de Gmail junto al mensaje.';
+$string['showlastname'] = 'Mostrar el apellido';
+$string['showlastnameinfo'] = 'Mostrar el apellido del autor de Gmail junto al mensaje.';
+$string['unreadmessages'] = 'Tiene {$a} mensajes sin leer en su bandeja de entrada.';
