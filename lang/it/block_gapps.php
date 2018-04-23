5f7c0f235da0ec0978bf35327158d7c778945384
David Castro <david.castro@blackboard.com>
2018-04-23 15:31:11 +0000

    INT-13192: Importing it lang

---
+++ lang/it/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Autorizza qui l\'accesso a Gmail per visualizzare i messaggi non letti.';
+$string['calendar'] = 'Calendario';
+$string['clientid'] = 'Client ID';
+$string['clientiddesc'] = 'Client ID dell\'account servizio nella Google Developers Console. Necessario solo se desideri visualizzare messaggi Gmail nel blocco Google Apps.';
+$string['compose'] = 'Componi e-mail';
+$string['domainnotconfigured'] = 'Il dominio Google Apps non è configurato nelle impostazioni amministratore di Google Apps.';
+$string['domainsetting'] = 'Dominio Google Apps';
+$string['domainsettingdesc'] = 'Questo è il dominio associato al tuo account Google Apps. Ad esempio, se effettui il login a Google Apps con l\'indirizzo \'foo@bar.com\', il tuo dominio è \'bar.com\'.';
+$string['drive'] = 'Unità';
+$string['gapps:addinstance'] = 'Aggiungi un nuovo block Google Apps';
+$string['gapps:myaddinstance'] = 'Aggiungi nuovo blocco Google Apps nella pagina La mia home';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Numero di messaggi non letti';
+$string['msgnumberunreadinfo'] = 'Il numero di messaggi Gmail non letti da visualizzare nel blocco Google Apps.';
+$string['newwinlink'] = 'Apri link nel popup';
+$string['newwinlinkinfo'] = 'Se selezionata, tutti i link nel blocco Google Apps si apriranno in una nuova finestra.';
+$string['nosubject'] = 'Nessun oggetto';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Mostra nome';
+$string['showfirstnameinfo'] = 'Mostra il nome dell\'autore Gmail accanto al suo messaggio.';
+$string['showlastname'] = 'Mostra cognome';
+$string['showlastnameinfo'] = 'Mostra il cognome dell\'autore Gmail accanto al suo messaggio.';
+$string['unreadmessages'] = 'Hai {$a} messaggi non letti nella posta in arrivo.';
