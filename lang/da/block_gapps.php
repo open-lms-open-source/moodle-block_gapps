257e737ce98be8d6a6a23c86e7b8a07b34fbf400
David Castro <david.castro@blackboard.com>
2018-04-23 15:34:10 +0000

    INT-13192: Importing da lang

---
+++ lang/da/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Godkend adgang til Gmail for at få vist dine ulæste meddelelser her.';
+$string['calendar'] = 'Kalender';
+$string['clientid'] = 'Klient-id';
+$string['clientiddesc'] = 'Dette er din tjenestekontos klient-id i Google Developers Console.  Det er kun nødvendigt, hvis du vil vise Gmail-beskeder i Google Apps-blokken.';
+$string['compose'] = 'Opret e-mail';
+$string['domainnotconfigured'] = 'Google Apps-domænet er ikke konfigureret i administratorindstillingerne for Google Apps.';
+$string['domainsetting'] = 'Google Apps-domæne';
+$string['domainsettingdesc'] = 'Dette domæne er knyttet til din Google Apps-konto. Hvis du for eksempel logger på Google Apps som \'foo@bar.com\', er dit domæne \'bar.com\'.';
+$string['drive'] = 'Drev';
+$string['gapps:addinstance'] = 'Tilføj en ny Google Apps-blok';
+$string['gapps:myaddinstance'] = 'Føj en ny Google Apps-blok til Min startside';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Antal ulæste meddelelser';
+$string['msgnumberunreadinfo'] = 'Det antal ulæste Gmail-meddelelser, der skal vises i Google Apps-blokken.';
+$string['newwinlink'] = 'Åbn links i pop op-vindue';
+$string['newwinlinkinfo'] = 'Alle links i Google Apps-blokken åbnes i et nyt vindue, hvis denne indstilling er aktiveret.';
+$string['nosubject'] = 'Intet emne';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Vis fornavn';
+$string['showfirstnameinfo'] = 'Vis Gmail-forfatterens fornavn ud for vedkommendes meddelelse.';
+$string['showlastname'] = 'Vis efternavn';
+$string['showlastnameinfo'] = 'Vis Gmail-forfatterens efternavn ud for vedkommendes meddelelse.';
+$string['unreadmessages'] = 'Du har {$a} ulæste meddelelser i din indbakke.';
