be05caabfa550c88c83ba99af15a204ec1b4a1ca
David Castro <david.castro@blackboard.com>
2018-04-23 15:43:20 +0000

    INT-13192: Importing zh_tw lang

---
+++ lang/zh_tw/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = '授權存取 Gmail，以便在此處檢視您的未讀訊息。';
+$string['calendar'] = '行事曆';
+$string['clientid'] = '用戶端代號';
+$string['clientiddesc'] = '此為您在 Google 開發人員控制台服務帳戶中的用戶端代號。這只有在您想於 Google Apps 區塊中顯示 Gmail 訊息時會用到。';
+$string['compose'] = '撰寫電子郵件';
+$string['domainnotconfigured'] = 'Google Apps 網域尚未在 Google Apps 管理設定值中配置。';
+$string['domainsetting'] = 'Google Apps 網域';
+$string['domainsettingdesc'] = '這是與您 Google Apps 帳戶關聯的網域。例如，若您是以 foo@bar.com 登入 Google Apps，您的網域即為 bar.com。';
+$string['drive'] = '磁碟機';
+$string['gapps:addinstance'] = '新增 Google Apps 區塊';
+$string['gapps:myaddinstance'] = '新增 Google Apps 區塊到「我的首頁」';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = '未讀訊息數量';
+$string['msgnumberunreadinfo'] = '在 Google Apps 區塊中顯示的未讀 Gmail 訊息數量。';
+$string['newwinlink'] = '在快顯視窗中開啟連結';
+$string['newwinlinkinfo'] = '如果啟用此設定，Google Apps 區塊中的所有連結就會在新視窗中開啟。';
+$string['nosubject'] = '沒有主旨';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = '顯示名字';
+$string['showfirstnameinfo'] = '在 Gmail 訊息旁顯示作者名字';
+$string['showlastname'] = '顯示姓氏';
+$string['showlastnameinfo'] = '在 Gmail 訊息旁顯示作者姓氏';
+$string['unreadmessages'] = '您的收件匣中有 {$a} 封未讀訊息。';
