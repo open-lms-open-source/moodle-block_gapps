e368be4bed6b96f7488ae667040a7d7a62c0bc8a
David Castro <david.castro@blackboard.com>
2018-06-13 14:04:46 +0000

    INT-13365: Importing tr lang

---
+++ lang/tr/block_gapps.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'Okunmamış iletilerinizi burada görmek için Gmail erişimine izin verin.';
+$string['calendar'] = 'Takvim';
+$string['clientid'] = 'İstemci kimliği';
+$string['clientiddesc'] = 'Bu, Google Developers Console’daki hizmet hesabınızın İstemci kimliğidir. Bu kimlik, yalnızca Gmail iletilerini Google Apps blokunda görüntülemek istediğinizde gereklidir.';
+$string['compose'] = 'E-posta oluştur';
+$string['domainnotconfigured'] = 'Google Apps etki alanı, Google Apps yönetici ayarlarında yapılandırılmadı.';
+$string['domainsetting'] = 'Google Apps etki alanı';
+$string['domainsettingdesc'] = 'Bu etki alanı Google Apps hesabınızla ilişkilendirilmiştir. Örneğin, Google Apps’e \'foo@bar.com\' olarak oturum açarsanız; etki alanınız da \'bar.com\' olur.';
+$string['drive'] = 'Drive';
+$string['gapps:addinstance'] = 'Yeni bir Google Apps bloku ekle';
+$string['gapps:myaddinstance'] = 'Ana sayfama yeni bir Google Apps bloku ekle';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'Okunmamış ileti sayısı';
+$string['msgnumberunreadinfo'] = 'Google Apps blokunda görüntülenecek okunmamış Gmail iletisi sayısı.';
+$string['newwinlink'] = 'Bağlantıları yeni pencerede aç';
+$string['newwinlinkinfo'] = 'Bu ayar etkinleştirilirse Google Apps blokundaki tüm bağlantılar yeni bir pencerede açılır.';
+$string['nosubject'] = 'Konu yok';
+$string['pluginname'] = 'Google Apps';
+$string['showfirstname'] = 'Adı göster';
+$string['showfirstnameinfo'] = 'Gmail yazarının adını, iletinin yanında gösterin.';
+$string['showlastname'] = 'Soyadını göster';
+$string['showlastnameinfo'] = 'Gmail yazarının soyadını, iletinin yanında gösterin.';
+$string['unreadmessages'] = 'Gelen kutunuzda {$a} okunmamış ileti var.';
