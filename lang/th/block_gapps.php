bf707c830683f0c354e9061c2ca2e83faf41bebe
Sam Chaffee <sam.chaffee@blackboard.com>
2019-10-04 13:05:41 -0600

    INT-15141: Import th lang

---
+++ lang/th/block_gapps.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['authorizeaccess'] = 'ให้สิทธิ์การเข้าถึง Gmail เพื่อดูข้อความที่ยังไม่ได้อ่านของคุณที่นี่';
+$string['calendar'] = 'ปฏิทิน';
+$string['clientid'] = 'ID ลูกค้า';
+$string['clientiddesc'] = 'นี่คือ ID ไคลเอนต์ของบัญชีบริการของคุณในคอนโซล Google Developers รหัสนี้จะจำเป็นเฉพาะเมื่อคุณต้องการให้แสดงข้อความ Gmail ในบล็อกของ Google Apps';
+$string['compose'] = 'เขียนอีเมล';
+$string['domainnotconfigured'] = 'โดเมน Google Apps ไม่ได้รับการกำหนดค่าในการตั้งค่าผู้ดูแลระบบ Google Apps';
+$string['domainsetting'] = 'โดเมน Google Apps';
+$string['domainsettingdesc'] = 'นี่คือโดเมนที่เชื่อมโยงกับบัญชี Google Apps ของคุณ ตัวอย่างเช่น หากคุณลงชื่อเข้าใช้ Google Apps ในชื่อ \'foo@bar.com\' โดเมนของคุณคือ \'bar.com\'';
+$string['drive'] = 'ไดรฟ์';
+$string['gapps:addinstance'] = 'เพิ่มบล็อก Google Apps ใหม่';
+$string['gapps:myaddinstance'] = 'เพิ่มบล็อก Google Apps ใหม่ไปที่หน้าแรกของฉัน';
+$string['gmail'] = 'Gmail';
+$string['msgnumberunread'] = 'จำนวนข้อความที่ยังไม่ได้อ่าน';
+$string['msgnumberunreadinfo'] = 'จำนวนข้อความ Gmail ที่ยังไม่ได้อ่านที่จะแสดงในบล็อกของ Google Apps';
+$string['newwinlink'] = 'เปิดลิงก์ในป็อปอัป';
+$string['newwinlinkinfo'] = 'ลิงก์ทั้งหมดในบล็อก Google Apps จะเปิดในหน้าต่างใหม่หากเปิดใช้งานการตั้งค่านี้';
+$string['nosubject'] = 'ไม่มีหัวเรื่อง';
+$string['pluginname'] = 'แอป Google';
+$string['showfirstname'] = 'แสดงชื่อ';
+$string['showfirstnameinfo'] = 'แสดงชื่อผู้เขียน Gmail ถัดจากข้อความของพวกเขา';
+$string['showlastname'] = 'แสดงนามสกุล';
+$string['showlastnameinfo'] = 'แสดงนามสกุลของผู้เขียน Gmail ถัดจากข้อความของพวกเขา';
+$string['unreadmessages'] = 'คุณมี {$a} ข้อความที่ยังไม่ได้อ่านในกล่องจดหมายของคุณ';
+$string['privacy:metadata:tool_googleadmin_users'] = 'เก็บข้อมูลประจำตัวของผู้ใช้งานสำหรับแอป Google';
+$string['privacy:metadata:tool_googleadmin_users:userid'] = 'ID ของผู้ใช้งาน';
+$string['privacy:metadata:tool_googleadmin_users:email'] = 'อีเมลของผู้ใช้งาน';
+$string['privacy:metadata:tool_googleadmin_users:password'] = 'รหัสผ่านของผู้ใช้งาน';
+$string['privacy:metadata:google_apps'] = 'ในการซิงค์กับแอป Google จะต้องทำการแลกเปลี่ยนข้อมูลผู้ใช้งานกับบริการดังกล่าว';
+$string['privacy:metadata:google_apps:userid'] = 'ID ผู้ใช้งาน Moodle กำลังซิงค์กับ Google Apps';
+$string['privacy:metadata:google_apps:email'] = 'อีเมลของผู้ใช้งานจะถูกซิงค์กับ Google Apps';
+$string['privacy:metadata:google_apps:password'] = 'รหัสผ่านของผู้ใช้งานจะถูกซิงค์กับ Google Apps';
+$string['privacy_stored_password'] = 'มีรหัสผ่านที่เก็บไว้';
