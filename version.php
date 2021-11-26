4f2f976ac678c3e92e65d6c12778763ebbc9de1d
Fabian Batioja <fabian.batioja@openlms.net>
2021-11-26 16:25:24 +0000

    INT-17365: 3.11.3 Bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2021060100;
+$plugin->version   = 2021112600;
 $plugin->requires  = 2021051700;
 $plugin->component = 'block_gapps';
 $plugin->cron      = 0;
-$plugin->release   = '3.11.2';
+$plugin->release   = '3.11.3';
