2390514328c497c37e446828e716b448061c3fde
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2023-03-22 20:03:46 +0000

    INT-18815: Importing nl lang

--- lang/nl/block_gapps.php
+++ lang/nl/block_gapps.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2022 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,11 +24,11 @@ defined('MOODLE_INTERNAL') || die();
 $string['authorizeaccess'] = 'Sta toegang tot Gmail toe om je ongelezen berichten hier te lezen.';
 $string['calendar'] = 'Kalender';
 $string['clientid'] = 'Client-ID';
-$string['clientiddesc'] = 'Dit is de client-ID van je service-account in de Google Developers Console. Dit is alleen nodig als je Gmail-berichten in het Google Apps-blok wilt weergeven.';
+$string['clientiddesc'] = 'Dit is de client-id van je service-account in de Google Developers Console. Dit is alleen nodig als je Gmail-berichten in het Google Apps-blok wilt weergeven.';
 $string['compose'] = 'Nieuwe e-mail opstellen';
 $string['domainnotconfigured'] = 'Het Google Apps-domein is niet geconfigureerd in de Google Apps-beheerinstellingen.';
 $string['domainsetting'] = 'Google Apps-domein';
-$string['domainsettingdesc'] = 'Dit is het domein dat bij je Google Apps-account hoort. Als je je bijvoorbeeld aanmeldt bij Google Apps als \'foo@bar.com\', dan is je domein \'bar.com\'.';
+$string['domainsettingdesc'] = 'Dit is het domein dat bij je Google Apps-account hoort. Als je je bijvoorbeeld aanmeldt bij Google Apps als &apos;foo@bar.com&apos;, dan is je domein &apos;bar.com&apos;.';
 $string['drive'] = 'Drive';
 $string['gapps:addinstance'] = 'Voeg een nieuw Google Apps-blok toe';
 $string['gapps:myaddinstance'] = 'Voeg een nieuw Google Apps-blok toe aan Mijn startpagina';
@@ -45,11 +45,11 @@ $string['showlastname'] = 'Achternaam weergeven';
 $string['showlastnameinfo'] = 'Toon de achternaam van de Gmail-auteur naast het betreffende bericht.';
 $string['unreadmessages'] = 'Je hebt {$a} ongelezen berichten in je inbox.';
 $string['privacy:metadata:tool_googleadmin_users'] = 'Hiermee worden gebruikersreferenties voor Google Apps opgeslagen';
-$string['privacy:metadata:tool_googleadmin_users:userid'] = 'De ID van de gebruiker';
+$string['privacy:metadata:tool_googleadmin_users:userid'] = 'De id van de gebruiker';
 $string['privacy:metadata:tool_googleadmin_users:email'] = 'Het e-mailadres van de gebruiker';
 $string['privacy:metadata:tool_googleadmin_users:password'] = 'Het wachtwoord van de gebruiker';
 $string['privacy:metadata:google_apps'] = 'Om te kunnen synchroniseren met Google Apps, moeten er gebruikersgegevens worden uitgewisseld met die service.';
-$string['privacy:metadata:google_apps:userid'] = 'De gebruikers-ID voor Moodle wordt gesynchroniseerd met Google Apps';
+$string['privacy:metadata:google_apps:userid'] = 'De gebruikers-id voor Moodle wordt gesynchroniseerd met Google Apps';
 $string['privacy:metadata:google_apps:email'] = 'Het e-mailadres van de gebruiker wordt gesynchroniseerd met Google Apps';
 $string['privacy:metadata:google_apps:password'] = 'Het wachtwoord van de gebruiker wordt gesynchroniseerd met Google Apps';
 $string['privacy_stored_password'] = 'Er is een wachtwoord opgeslagen';
