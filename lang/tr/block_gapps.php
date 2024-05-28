a6bf889321f07ff1251153ed13d4db649010b216
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:18 +0000

    INT-19523: Importing tr lang

--- lang/tr/block_gapps.php
+++ lang/tr/block_gapps.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -28,7 +28,7 @@ $string['clientiddesc'] = 'Bu, Google Developers Console\'daki hizmet hesabını
 $string['compose'] = 'E-posta oluştur';
 $string['domainnotconfigured'] = 'Google Apps etki alanı, Google Apps yönetici ayarlarında yapılandırılmadı.';
 $string['domainsetting'] = 'Google Apps etki alanı';
-$string['domainsettingdesc'] = 'Bu etki alanı Google Apps hesabınızla ilişkilendirilmiştir. Örneğin, Google Apps’e \'foo@bar.com\' olarak oturum açarsanız; etki alanınız da \'bar.com\' olur.';
+$string['domainsettingdesc'] = 'Bu etki alanı Google Apps hesabınızla ilişkilendirilmiştir. Örneğin, Google Apps\'e \'foo@bar.com\' olarak oturum açarsanız; etki alanınız da \'bar.com\' olur.';
 $string['drive'] = 'Drive';
 $string['gapps:addinstance'] = 'Yeni bir Google Apps bloku ekle';
 $string['gapps:myaddinstance'] = 'Ana sayfama yeni bir Google Apps bloku ekle';
