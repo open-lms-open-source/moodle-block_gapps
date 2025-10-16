8ecedb45c1973e864c7e6e393fa8d09cf0297854
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2025-10-16 21:31:43 +0000

    INT-21443: Importing tr lang

--- lang/tr/block_gapps.php
+++ lang/tr/block_gapps.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2025 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,11 +24,11 @@ defined('MOODLE_INTERNAL') || die();
 $string['authorizeaccess'] = 'Okunmamış iletilerinizi burada görmek için Gmail erişimine izin verin.';
 $string['calendar'] = 'Takvim';
 $string['clientid'] = 'İstemci Kimliği';
-$string['clientiddesc'] = 'Bu, Google Developers Console\'daki hizmet hesabınızın İstemci kimliğidir. Bu kimlik, yalnızca Gmail iletilerini Google Apps blokunda görüntülemek istediğinizde gereklidir.';
+$string['clientiddesc'] = 'Bu, Google Developers Console’daki hizmet hesabınızın İstemci kimliğidir. Bu kimlik, yalnızca Gmail iletilerini Google Apps blokunda görüntülemek istediğinizde gereklidir.';
 $string['compose'] = 'E-posta oluştur';
 $string['domainnotconfigured'] = 'Google Apps etki alanı, Google Apps yönetici ayarlarında yapılandırılmadı.';
 $string['domainsetting'] = 'Google Apps etki alanı';
-$string['domainsettingdesc'] = 'Bu etki alanı Google Apps hesabınızla ilişkilendirilmiştir. Örneğin Google Apps\'te \'foo@bar.com\' olarak oturum açarsanız etki alanınız da \'bar.com\' olur.';
+$string['domainsettingdesc'] = 'Bu etki alanı Google Apps hesabınızla ilişkilendirilmiştir. Örneğin Google Apps\'te \'foo@bar.com\' olarak oturum açıyorsanız etki alanınız da \'bar.com\' olur.';
 $string['drive'] = 'Drive';
 $string['gapps:addinstance'] = 'Yeni bir Google Apps bloku ekle';
 $string['gapps:myaddinstance'] = 'Ana sayfama yeni bir Google Apps bloku ekle';
