b6d3f9ce09794baf161d19959668867ae7fe6316
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2026-06-09 15:27:20 -0500

    INT-21630: 5.1 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2026032600;
-$plugin->requires  = 2024100700;
+$plugin->version   = 2026060900;
+$plugin->requires  = 2025100600;
 $plugin->component = 'block_gapps';
 $plugin->cron      = 0;
-$plugin->release   = '4.5.4';
+$plugin->release   = '5.1.4';
